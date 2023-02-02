7396130bca99854e67260e41bbb4d4ff0371a32d
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2023-02-02 21:38:13 +0000

    Add basic setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FLAN',
+    version='0.0',
+    description='flan',
+    author='John Doe',
+    author_email='jdoe@example.com',
+    packages=['flan'],
+    install_requires=[
+    ],
+)
